ae7cfee3026fca8aea194c1c718f5d491407e091
ramge@mnixos <axel@ramge.de>
2026-03-22 13:00:42 +0100

    removed broken tmux start

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -3,9 +3,30 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
 
+-- Neu für die stabile Platzierung
+import XMonad.Actions.SpawnOn (spawnOn, manageSpawn)
+
 import XMonad.Layout.MultiToggle (mkToggle, single, Toggle(..))
 import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL))
 import XMonad.Hooks.ManageDocks (ToggleStruts(..))
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+    [ manageSpawn -- WICHTIG: Erlaubt spawnOn seine Arbeit zu tun
+    , className =? "keepassxc"        --> doShift "1"
+    , className =? "chromium-browser" --> doShift "1"
+    ]
+
+myStartupHook :: X ()
+myStartupHook = do
+    -- DPI fixen
+    spawn "fix-dpi"
+    -- Programme direkt auf Workspaces zwingen
+    spawnOn "1" "keepassxc"
+    spawnOn "1" "ghostty --title=ghostty-1"
+    spawnOn "1" "chromium"
+    spawnOn "2" "ghostty --title=ghostty-2"
+    spawnOn "3" "ghostty --title=ghostty-3"
 
 main :: IO ()
 main = xmonad . ewmhFullscreen . ewmh =<< xmobar (def
@@ -13,15 +34,17 @@ main = xmonad . ewmhFullscreen . ewmh =<< xmobar (def
     , terminal          = "ghostty"
     , focusFollowsMouse = False
 
-    -- Das optische Tuning: Tahoe-Style
-    , borderWidth       = 5                    -- Dreimal so dick wie vorher
-    , focusedBorderColor= "#E58C7A"            -- Tahoe-Rot (Sanftes Koralle/Ziegelrot)
-    , normalBorderColor = "#2B3A42"            -- Tahoe-Dunkelblau/Tiefseegrau
+    , borderWidth       = 5
+    , focusedBorderColor= "#E58C7A"
+    , normalBorderColor = "#2B3A42"
 
     , layoutHook        = mkToggle (single NBFULL) (layoutHook def)
+    , manageHook        = myManageHook <+> manageHook def
+    , startupHook       = myStartupHook
     }
     `additionalKeysP`
     [ ("M-S-ü", spawn "maim -s | xclip -selection clipboard -t image/png")
     , ("M-p",   spawn "rofi -show drun")
     , ("M-z",   sendMessage (Toggle NBFULL) >> sendMessage ToggleStruts)
+    , ("M-f",   spawn "fix-dpi") -- Falls du manuell nachfeuern willst
     ])
